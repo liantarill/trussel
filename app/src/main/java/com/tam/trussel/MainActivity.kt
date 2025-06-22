96f8dc19353a51fd7aea5b464d8de602879388b8
liantarill <yusufherliananantaril@gmail.com>
2025-06-22 21:25:50 +0700

    membuat struktur project

--- app/src/main/java/com/tam/trussel/MainActivity.kt
+++ app/src/main/java/com/tam/trussel/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tam.trussel
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,21 +16,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        val nav = findViewById<BottomNavigationView>(R.id.nav_view)
+        nav.itemIconTintList = null
+        nav.itemTextColor = null
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_forYou, R.id.navigation_sell,R.id.navigation_chat, R.id.navigation_profile
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home -> navView.visibility = View.VISIBLE
+                R.id.navigation_forYou -> navView.visibility = View.VISIBLE
+                R.id.navigation_sell -> navView.visibility = View.VISIBLE
+                R.id.navigation_chat -> navView.visibility = View.VISIBLE
+                R.id.navigation_profile -> navView.visibility = View.VISIBLE
+                else -> navView.visibility = View.GONE
+            }
+        }
+
     }
 }
